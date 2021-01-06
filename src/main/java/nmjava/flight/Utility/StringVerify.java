680aed644777d7896b0a89b4fde0aa25c4fabadc
NXLuan <xuanluan1412@gmail.com>
2021-01-06 20:21:03 +0700

    Luan update

--- src/main/java/nmjava/flight/Utility/StringVerify.java
+++ src/main/java/nmjava/flight/Utility/StringVerify.java
@@ -5,15 +5,28 @@
  */
 package nmjava.flight.Utility;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author xuanluan
  */
 public class StringVerify {
-    private static String PATTERN;
-    
-    public static boolean isValidEmail(){
-        PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";// cấu trúc 1 email thông thường
+
+    public static boolean isValidEmail(String text) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        return Pattern.compile(EMAIL_PATTERN).matcher(text).matches();
+    }
+
+    public static boolean isValidPhoneNumber(String text) {
+        String PHONE_NUMBER_PATTERN = "^(0\\d{9})|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}";
+        return Pattern.compile(PHONE_NUMBER_PATTERN).matcher(text).matches();
+    }
+
+    public static boolean isValidCMND(String text) {
+        String CMND_PATTERN = "[0-9]{9}";
+        return Pattern.compile(CMND_PATTERN).matcher(text).matches();
     }
 }
